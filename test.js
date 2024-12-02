6279aefe3080ff5e87b91a3e95e9927c9febc657
Andrew <andreavigolini@gmail.com>
2024-12-02 20:08:29 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+import { printColored } from './terminalGraphics.js';
+
+printColored('Hello, this is a red message!', 'red');
+printColored('This is a bold message!', 'bold');
+printColored('This message is green!', 'green');
