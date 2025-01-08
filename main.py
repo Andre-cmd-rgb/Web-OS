edbb354e64813210fa26ea8397bee53bbd8d5439
Andrew <85292605+Andre-cmd-rgb@users.noreply.github.com>
2025-01-08 14:43:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+# this is a flask app just to serve the index.html for dev 
 @app.route("/")
 def home():
     return render_template("index.html")
